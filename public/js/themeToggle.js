7dcd3d6afbea4ede8683ab1dd3176adcbe41e913
AlexDDks <magarciaa92@gmail.com>
2024-07-10 17:02:35 -0600

    updating light mode

--- public/js/themeToggle.js
+++ public/js/themeToggle.js
@@ -6,19 +6,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const darkModeIcon = '<i class="fa-solid fa-moon"></i>';  // Icono para modo oscuro
     const lightModeIcon = '<i class="fa-solid fa-sun"></i>';  // Icono para modo claro
 
-    // Inicializar el icono según el modo actual
-    if (document.documentElement.classList.contains('light-mode')) {
-        toggleButton.innerHTML = darkModeIcon;
-    } else {
-        toggleButton.innerHTML = lightModeIcon;
+    // Función para inicializar el modo según localStorage
+    function initializeTheme() {
+        const theme = localStorage.getItem('theme');
+        if (theme === 'light-mode') {
+            document.documentElement.classList.add('light-mode');
+            toggleButton.innerHTML = darkModeIcon;
+        } else {
+            document.documentElement.classList.remove('light-mode');
+            toggleButton.innerHTML = lightModeIcon;
+        }
     }
+
+    // Inicializar el tema al cargar la página
+    initializeTheme();
 
     toggleButton.addEventListener('click', function() {
         document.documentElement.classList.toggle('light-mode');
         if (document.documentElement.classList.contains('light-mode')) {
             toggleButton.innerHTML = darkModeIcon;
+            localStorage.setItem('theme', 'light-mode');
         } else {
             toggleButton.innerHTML = lightModeIcon;
+            localStorage.setItem('theme', 'dark-mode');
         }
     });
 });
